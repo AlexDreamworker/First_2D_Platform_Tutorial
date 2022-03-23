882b092ab4e2e4e4daa093ba2e639d7c076166b5
Alex Dreamworker <alexdreamworker@gmail.com>
2022-03-23 13:56:35 +0300

    Multi-Weapon Combat System

--- Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
+++ Assets/Scripts/Player/PlayerFiniteStateMachine/Player.cs
@@ -19,7 +19,9 @@ public class Player : MonoBehaviour
     public PlayerLedgeClimpState LedgeClimpState { get; private set; }
     public PlayerDashState DashState { get; private set; }
     public PlayerCrouchIdleState CrouchIdleState { get; private set; }
-    public PlayerCrouchMoveState CrouchMoveState { get; private set; }
+    public PlayerCrouchMoveState CrouchMoveState { get; private set; } 
+    public PlayerAttackState PrimaryAttackState { get; private set; } 
+    public PlayerAttackState SecondaryAttackState { get; private set; } 
 
     [SerializeField] private PlayerData playerData;
     #endregion
@@ -30,6 +32,7 @@ public class Player : MonoBehaviour
     public Rigidbody2D RB { get; private set; }
     public Transform DashDirectionIndicator { get; private set; }
     public BoxCollider2D MovementCollider { get; private set; }
+    public PlayerInventory Inventory { get; private set; }
     #endregion
 
     #region Check Transforms
@@ -65,6 +68,8 @@ public class Player : MonoBehaviour
         DashState = new PlayerDashState(this, StateMachine, playerData, "inAir");
         CrouchIdleState = new PlayerCrouchIdleState(this, StateMachine, playerData, "crouchIdle");
         CrouchMoveState = new PlayerCrouchMoveState(this, StateMachine, playerData, "crouchMove");
+        PrimaryAttackState = new PlayerAttackState(this, StateMachine, playerData, "attack");
+        SecondaryAttackState = new PlayerAttackState(this, StateMachine, playerData, "attack");
     }
 
     private void Start()
@@ -74,9 +79,12 @@ public class Player : MonoBehaviour
         RB = GetComponent<Rigidbody2D>();
         DashDirectionIndicator = transform.Find("DashDirectionIndicator");
         MovementCollider = GetComponent<BoxCollider2D>();
+        Inventory = GetComponent<PlayerInventory>();
 
         FacingDirection = 1;
 
+        PrimaryAttackState.SetWeapon(Inventory.weapons[(int)CombatInputs.primary]);
+        //SecondaryAttackState.SetWeapon(Inventory.weapons[(int)CombatInputs.primary]);
         StateMachine.Initialize(IdleState);
     }
 
