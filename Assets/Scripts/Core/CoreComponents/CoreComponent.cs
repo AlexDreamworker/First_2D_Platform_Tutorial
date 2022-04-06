07cf916f0916d0619fd5352365e60f2649bbfc69
Alex Dreamworker <alexdreamworker@gmail.com>
2022-04-06 19:15:36 +0300

    Stats

--- Assets/Scripts/Core/CoreComponents/CoreComponent.cs
+++ Assets/Scripts/Core/CoreComponents/CoreComponent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoreComponent : MonoBehaviour
+public class CoreComponent : MonoBehaviour, ILogicUpdate
 {
     protected Core core;
 
@@ -10,11 +10,9 @@ public class CoreComponent : MonoBehaviour
     {
         core = transform.parent.GetComponent<Core>();
 
-        if (core == null) 
-        { 
-            Debug.LogError("There is no CORE on the parent"); 
-        }
+        if (core == null) { Debug.LogError("There is no CORE on the parent"); }
+        core.AddComponent(this);
     }
 
-    
+    public virtual void LogicUpdate() { }
 }
